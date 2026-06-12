c1f7a7d9b4ee3fcbd884ff9d3d9116ccf08faf15
Saksham Verma <212260728+sakmv@users.noreply.github.com>
2026-06-12 11:42:10 +0530

    Completed reccursive chunking function Also added merge function

--- text_splitter.py
+++ text_splitter.py
@@ -1,39 +1,7 @@
 text = open("input.txt", "r").read()
-## RECCURSIVE CHUNKING TEXT-SPLITTER FROM SCRATCH
-separator=["\n\n\n","\n\n","\n","."," ",""]
-def rec_chunk(text,chunk_size,chunk_overlap,separator):
-    final=[]
-    i=0
-    sep=separator[i]
-    chunks=sen_bound(text,chunk_size,sep,chunk_overlap)
-    for chunk in chunks:
-       if(len(chunk)>chunk_size):
-          if(i+1<len(separator)):
-             chunks.replace(chunk,rec_chunk(chunk,chunk_size,chunk_overlap,separator[i+1:]))
-          else:
-             print("no fallback separator reamining")
-    
 
+#SENTENCE BOUNDARY CHUNKING FROM SCRATCH:
 
-## SUDO CODE FOR OTHER TEXT SPLITTING STRATEGIES FROM SCRATCH:
-
-#STRATEGRY 1. FIXED SIZE CHUNKING:
-
-
-# def fixed_size(text,chunk_size,chunk_overlap):
-#     if(chunk_overlap>=chunk_size):
-#         print("Overlap cannot be greater than or equal to chunk size")
-#         return []
-#     chunks = []
-#     i=0
-#     while i < len(text):
-#         end=i+chunk_size
-#         chunks.append(text[i:end])
-#         i=end-chunk_overlap
-#     print(chunks)
-#     return chunks
-
-#STRATEGY 2. SENTENCE BOUNDARY CHUNKING:
 def sen_bound(text, chunk_size, sep, overlap=1):
      chunks = []
      sen = []
@@ -67,5 +35,64 @@ def sen_bound(text, chunk_size, sep, overlap=1):
 
      return chunks
     
-result=sen_bound(text, 500, separator[3], 1)
-print(result[0])
+## RECCURSIVE CHUNKING TEXT-SPLITTER FROM SCRATCH
+
+separator=["\n\n\n","\n\n","\n","."," "]
+
+#MERGE FUNCTION FOR FINAL MERGING 
+
+def merge(chunks,chunk_size,chunk_overlap):
+    buffer=[]
+    merged=[]
+    for chunk in chunks:
+      if(len("".join(buffer+[chunk]))<=chunk_size):
+         buffer.append(chunk)
+      else:
+         if buffer:
+            merged.append(" ".join(buffer))
+         overlap = buffer[-chunk_overlap:] if chunk_overlap else []
+         if len(" ".join(overlap + [chunk])) <= chunk_size:
+            buffer = overlap  
+         else:
+                buffer = []
+         buffer.append(chunk)
+    if buffer:
+       merged.append(" ".join(buffer))
+    return merged
+
+def rec_chunk(text,chunk_size,chunk_overlap,separator):
+    final=[]
+    i=0
+    sep=separator[i]
+    chunks=sen_bound(text,chunk_size,sep,chunk_overlap)
+    for idx,chunk in enumerate(chunks):
+       if(len(chunk)>chunk_size):
+          if(i+1<len(separator)):
+             final.extend(rec_chunk(chunk,chunk_size,chunk_overlap,separator[i+1:]))
+             
+          else:
+             print("no")
+             final.append(chunk)
+       else:
+          final.append(chunk)
+          
+    return merge(final,chunk_size,chunk_overlap)
+
+chunk=rec_chunk(text,500,50,separator)
+print(chunk[0])
+
+
+# STRATEGRY : FIXED SIZE CHUNKING (NOT GOOD FOR RAG AS IT DOESNT RESPECT CONTEXT)
+
+# def fixed_size(text,chunk_size,chunk_overlap):
+#     if(chunk_overlap>=chunk_size):
+#         print("Overlap cannot be greater than or equal to chunk size")
+#         return []
+#     chunks = []
+#     i=0
+#     while i < len(text):
+#         end=i+chunk_size
+#         chunks.append(text[i:end])
+#         i=end-chunk_overlap
+#     print(chunks)
+#     return chunks
